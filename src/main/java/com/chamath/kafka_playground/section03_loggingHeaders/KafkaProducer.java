b04e4998d022bd6834963f38ee3faddd00e0647d
Chamath Heshan <chamathheshan076@gmail.com>
2025-01-10 18:01:18 +0530

    add logging headers for consumer,producer

---
+++ src/main/java/com/chamath/kafka_playground/section03_loggingHeaders/KafkaProducer.java
@@ -0,0 +1,52 @@
+package com.chamath.kafka_playground.section03_loggingHeaders;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.Map;
+
+//kafka producer using reactor-kafka
+public class KafkaProducer {
+
+
+public static final Logger log= LoggerFactory.getLogger(KafkaProducer.class);
+
+public static void main(String[] args) {
+
+
+    var producerConfig=Map.<String,Object>of(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+    );
+
+    var options= SenderOptions.<String, String>create(producerConfig);
+
+    var flux= Flux.range(1,10)
+            .map(KafkaProducer::createSenderRecord);
+
+    KafkaSender.create(options)
+            .send(flux)
+            .doOnNext(r->log.info(" correlation id {}", r.correlationMetadata()))
+            .subscribe();
+
+}
+
+private static SenderRecord<String,String,String> createSenderRecord(Integer i){
+    var headers= new RecordHeaders();
+    headers.add("client-id ","some-client".getBytes());
+    headers.add("tracing-id" , "123".getBytes());
+
+    var pr= new ProducerRecord<>("order-events", null,i.toString(), "order-" +i,headers);
+    return SenderRecord.create(pr,pr.key());
+}
+
+}
